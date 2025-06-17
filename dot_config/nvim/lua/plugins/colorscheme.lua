01fa7e6866d88a91503b62b1054817dd7cf6fa14
Wavefire5201 <1541053496ee@gmail.com>
2025-06-17 13:30:37 -0500

    laptop stuff

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,8 @@
 return {
   "catppuccin/nvim",
-  lazy = true,
   name = "catppuccin",
+  lazy = false,
+  priority = 1000,
   opts = {
     flavour = "mocha",
     transparent_background = true,
@@ -55,10 +56,14 @@ return {
       end,
     },
   },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+  end,
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin",
+  --   },
+  -- },
 }
