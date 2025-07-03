32fb863ec0e3243558501294fcca510242c7fb51
Wavefire5201 <70147423+Wavefire5201@users.noreply.github.com>
2025-07-03 02:50:37 -0500

    chore: change floating kitty size for laptop

--- dot_config/hypr/scripts/executable_toggle_kitty.sh
+++ dot_config/hypr/scripts/executable_toggle_kitty.sh
@@ -27,7 +27,6 @@ if [ -z "$window_id" ]; then
     hyprctl dispatch setfloating address:$window_id
     sleep 0.1
     hyprctl dispatch centerwindow address:$window_id
-    hyprctl dispatch resizewindowpixel exact 50% 50%,address:$window_id
     hyprctl dispatch focuswindow address:$window_id
   fi
 else
